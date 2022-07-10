03b804fa154c4b3bcb0b29cae1e2125da06dbe74
pauloricardoma <pauloricardo.m.almeida@gmail.com>
2022-07-10 03:36:58 -0300

    feat(toast):implementation

--- src/components/Toast/ToastContainer/index.js
+++ src/components/Toast/ToastContainer/index.js
@@ -1,31 +1,43 @@
 import { useEffect, useState } from 'react';
-import ToastMessage from '../ToastMessage';
+
 import { Container } from './styles';
+
+import ToastMessage from '../ToastMessage';
+
+import { toastEventManager } from '../../../utils/toast';
 
 export default function ToastContainer() {
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
-    function handleAddToast(event) {
-      const { type, text } = event.detail;
-
+    function handleAddToast({ type, text }) {
       setMessages((prevState) => [
         ...prevState,
         { id: Math.random(), type, text },
       ]);
     }
 
-    document.addEventListener('addtoast', handleAddToast);
+    toastEventManager.on('addtoast', handleAddToast);
 
     return () => {
-      document.removeEventListener('addtoast', handleAddToast);
+      toastEventManager.removeListener('addtoast', handleAddToast);
     };
   }, []);
+
+  function handleRemoveMessage(id) {
+    setMessages((prevState) => prevState.filter(
+      (message) => message.id !== id,
+    ));
+  }
 
   return (
     <Container>
       {messages.map((message) => (
-        <ToastMessage key={message.id} type={message.type} text={message.text} />
+        <ToastMessage
+          key={message.id}
+          message={message}
+          onRemoveMessage={handleRemoveMessage}
+        />
       ))}
     </Container>
   );
