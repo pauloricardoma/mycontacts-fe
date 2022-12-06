b756234ed47c998578bcb8486f93306e0839e63a
Paulo Ricardo <pauloricardo.m.almeida@gmail.com>
2022-12-06 00:27:26 -0300

    data mapper

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -4,19 +4,13 @@ import PageHeader from '../../components/PageHeader';
 import ContactForm from '../../components/ContactForm';
 import ContactsService from '../../services/ContactsService';
 import toast from '../../utils/toast';
+import ContactMapper from '../../services/mappers/ContactMapper';
 
 export default function NewContact() {
   const contactFormRef = useRef(null);
 
-  async function handleSubmit(formData) {
+  async function handleSubmit(contact) {
     try {
-      const contact = {
-        name: formData.name,
-        email: formData.email,
-        phone: formData.phone,
-        category_id: formData.categoryId,
-      };
-
       await ContactsService.createContact(contact);
 
       contactFormRef.current.resetFields();
